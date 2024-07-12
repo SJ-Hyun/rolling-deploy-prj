fa8f30f2aaa489d1d3bc707ff5967895a50cd15c
SJ-Hyun <89343742+SJ-Hyun@users.noreply.github.com>
2024-07-12 14:24:56 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
         return input;
     }
 
-    @RequestMapping("/hello")
+    //@RequestMapping("/hello")
     public String hello() {
         return "파이프라인 웹훅 테스트";
     }
